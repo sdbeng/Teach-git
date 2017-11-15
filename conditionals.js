8a5461b579234a050b247c8b9d98bedaf13258bc
Sergio Barrera <sdbeng@gmail.com>
2017-11-15 06:28:09 -0800

    Add conditionals project

---
+++ conditionals.js
@@ -0,0 +1,4 @@
+/* use if, else if, else, switch, and ternary syntax to control the flow of code execution in JavaScript. Programmers use these keywords to write conditional statements that enable programs to execute different blocks of code depending on specified conditions.
+
+Control flow enables developers to write programs that make decisions. */
+
