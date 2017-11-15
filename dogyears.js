cada1d53e5c3052a8b66bbf3c3d79c0d391366ce
Sergio Barrera <sdbeng@gmail.com>
2017-11-15 06:17:16 -0800

    Add dogyears project

---
+++ dogyears.js
@@ -0,0 +1,9 @@
+const myAge = 34
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+let myAgeInDogYears = earlyYears + laterYears
+let myName = prompt('What is your name?');
+myName = myName.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in dog years.`);
